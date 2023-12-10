eab1ca06ffba5e378ede09ce0616806655e7e582
Aleksander <olek.drwal@gmail.com>
2023-12-10 23:54:48 +0100

    changed filename

--- main.py
+++ main.py
@@ -1,71 +1,104 @@
-import numpy as np
+from typing import Dict, List, Union
 import pandas as pd
+import numpy as np
+from collections import Counter
+
+from utils import calculate_entropy_u, load_data_frame_u, split_train_data_u
 
 
-def load_csv_into_pandas():
-    col_names = ["Day", "Outlook", "Temp", "Humidity", "Wind", "PlayTennis"]
-    data = pd.read_csv("test-data.csv", skiprows=1, header=None, names=col_names)
-    return data
+class Node:
+    def __init__(self, most_common_attribute_choice: str):
+        self.decision_attribute: str = ""
+        self.decision_attribute_choices: Dict[str, Union[Node, str]] = {}
+        self.most_common_attribute_choice = most_common_attribute_choice
+
+    def add_child(self, child_name: str, child: Union["Node", str]):
+        self.decision_attribute_choices[child_name] = child
 
 
-def calculate_entropy(data):
-    entropy = 0
-    play_tennis = data.keys()[-1]
-    values = data[play_tennis].unique()
-    for value in values:
-        fraction = data[play_tennis].value_counts()[value] / len(data[play_tennis])
-        entropy += -fraction * np.log2(fraction)
-    return entropy
+class Tree:
+    def __init__(self, data_path: str):
+        self.data = load_data_frame_u(data_path)
+        train_data, test_data = split_train_data_u(self.data, train_size=3 / 5)
+        self.test_data = test_data
+        self.root = self._build_tree(train_data)
+        self.accuracy = self._calculate_accuracy(test_data, self.root)
 
+    def classify(self, instance: pd.Series, tree: Node):
+        if isinstance(tree, str):
+            return tree
 
-def get_most_informative_feature(data):
-    information_gain = []
-    for key in data.keys()[:-1]:
-        information_gain.append(caluclate_gain_for_attribute(data, key))
-    return data.keys()[:-1][np.argmax(information_gain)]
+        attribute = tree.decision_attribute
+        attribute_value = instance[attribute]
+        if attribute_value not in tree.decision_attribute_choices:
+            return tree.most_common_attribute_choice
+        return self.classify(
+            instance, tree.decision_attribute_choices[attribute_value]  # type: ignore
+        )
 
+    def _calculate_gain(self, data: pd.DataFrame, attribute: str):
+        total_entropy = calculate_entropy_u(data)
+        values, counts = np.unique(data[attribute], return_counts=True)
+        values_entropy = sum(
+            (counts[i] / np.sum(counts))
+            * calculate_entropy_u(data.where(data[attribute] == values[i]).dropna())
+            for i in range(len(values))
+        )
+        return total_entropy - values_entropy
 
-def caluclate_gain_for_attribute(data, attribute):
-    gain = calculate_entropy(data)
-    attribute_values = data[attribute].unique()
-    for attribute_value in attribute_values:
-        sub_data = data[data[attribute] == attribute_value]
-        gain -= len(sub_data) / len(data) * calculate_entropy(sub_data)
-    return gain
+    def _calculate_accuracy(self, test_data: pd.DataFrame, tree: Node):
+        correct = 0
+        for row in test_data.iloc:
+            if self.classify(row, tree) == row[-1]:
+                correct += 1
+        return correct / len(test_data)
 
+    def _get_most_informative_attribute(self, data: pd.DataFrame) -> str:
+        attribute_gains = [
+            (attribute, self._calculate_gain(data, attribute))
+            for attribute in data.columns[:-1]
+        ]
 
-def build_tree(data, tree=None):
-    if tree is None:
-        tree = {}
-    feature = get_most_informative_feature(data)
-    attribute_values = np.unique(data[feature])
-    for value in attribute_values:
-        sub_data = data[data[feature] == value]
-        class_counts = sub_data["PlayTennis"].value_counts()
-        if len(class_counts) == 1:
-            tree[feature] = class_counts.index[0]
-        else:
-            tree[feature] = build_tree(sub_data)
-    return tree
+        return max(attribute_gains, key=lambda x: x[1])[0]
+
+    def _build_tree(self, data: pd.DataFrame, tree: Node | None = None):
+        # Get the feature with the highest information gain
+        attribute = self._get_most_informative_attribute(data)
+
+        # If all attrubites have only one value, return the most common class
+        each_col_ony_one_val = [
+            data[col_name].nunique() == 1 for col_name in data.columns[:-1]
+        ]
+        if all(each_col_ony_one_val):
+            # Counter - buffed dictionary
+            return Counter(data[data.columns[-1]]).most_common(1)[0][0]
+
+        # If class col has only one value, return it
+        if len(np.unique(data[data.columns[-1]])) == 1:
+            return np.unique(data[data.columns[-1]])[0]
+
+        # Continue building the tree
+        if tree is None:
+            tree = Node(Counter(data[data.columns[-1]]).most_common(1)[0][0])
+
+        attr_unique_values = np.unique(data[attribute])
+        for value in attr_unique_values:
+            sub_data = data.where(data[attribute] == value).dropna()
+            classes_col = sub_data[sub_data.columns[-1]]  # Get the classes column
+            class_value, counts = np.unique(classes_col, return_counts=True)
+            if len(counts) == 1:
+                tree.decision_attribute = attribute
+                tree.add_child(value, class_value[0])
+            else:
+                tree.decision_attribute = attribute
+                tree.add_child(value, self._build_tree(sub_data))
+
+        return tree
 
 
 def main():
-    data = load_csv_into_pandas()
-    # cut day column
-    data = data.drop("Day", axis=1)
-    print("Entropy of the dataset:", calculate_entropy(data))
-    print("entropy of sunny day:", calculate_entropy(data[data["Outlook"] == "Sunny"]))
-    print(
-        "entropy of overcast day:",
-        calculate_entropy(data[data["Outlook"] == "Overcast"]),
-    )
-    print("entropy of rainy day:", calculate_entropy(data[data["Outlook"] == "Rain"]))
-    print(
-        "calculating gain for outlook:", caluclate_gain_for_attribute(data, "Outlook")
-    )
-    print("Most informative feature:", get_most_informative_feature(data))
-    tree = build_tree(data)
-    print("Decision Tree:", tree)
+    tree = Tree("data/breast-cancer/breast-cancer.data")
+    print(tree.accuracy)
 
 
 if __name__ == "__main__":
